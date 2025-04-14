349cb03358a776da6ea16b7c7887e08ca9ceabe7
sofia <195935617+sofiasanvi@users.noreply.github.com>
2025-04-14 13:50:27 +0200

    changing the convert_pdb_to_pdbqt.py & creating a new env autodock_py2.yml

--- workflow/scripts/convert_pdb_to_pdbqt.py
+++ workflow/scripts/convert_pdb_to_pdbqt.py
@@ -1,31 +1,25 @@
-from __future__ import print_function  # Ensures compatibility for print()
-
 import os
 import subprocess
 
-# Define the input and output directories
-input_dir = 'results/gen/'
-output_dir = 'results/converted_pdbqt/'
+input_dir = 'results/nat/'
+output_dir = 'results/converted_pdbqt_natural/'
 
-# Make sure the output directory exists
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# Loop through all PDB files in the input directory
 for filename in os.listdir(input_dir):
-    if filename.endswith('.pdb'):  # Only process PDB files
+    if filename.endswith('.pdb'):
         input_file = os.path.join(input_dir, filename)
         output_file = os.path.join(output_dir, filename.replace('.pdb', '.pdbqt'))
 
-        # Run the prepare_receptor4.py script for each file
         command = [
-            'python2', '/home/moa/miniforge3/envs/bb103x/CCSBpckgs/AutoDockTools/Utilities24/prepare_receptor4.py',
-            '-r', input_file, '-o', output_file
+            'prepare_receptor4.py', 
+            '-r', input_file,
+            '-o', output_file
         ]
-        
-        # Execute the command
+
         try:
-            subprocess.check_call(command)  # Use check_call instead of run
-            print("Successfully converted {} to PDBQT.".format(filename))  # Using .format() for string formatting
+            subprocess.check_call(command)
+            print("Successfully converted {} to PDBQT.".format(filename))
         except subprocess.CalledProcessError as e:
-            print("Error processing {}: {}".format(filename, e))  # Using .format() for string formatting
+            print("Error processing {}: {}".format(filename, e))
